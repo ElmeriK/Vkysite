e23e1dca7a3b4b4eb00fad7f08e5b06bcbaa7e8d
Elmeri Kuismin <elmeri.kuismin@gmail.com>
2021-03-11 13:02:38 +0200

    TJ db changed from a json file to a MSSQL server

--- vkySiteBack/back.js
+++ vkySiteBack/back.js
@@ -2,24 +2,36 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fs = require('fs');
-const foodRSS = require('./foodRSS')
+const foodRSS = require('./foodRSS');
 const bodyParser = require('body-parser');
+const sql = require('mssql');
 
-const backURL = "http://localhost:3001"
+const backURL = "http://localhost:3001";
+const sqlConfig = {
+    user: 'back',
+    password: 'Password1',
+    server: 'DESKTOP-S5SSUD6',
+    database: 'Vkysite',
+
+    "options": {
+      "encrypt": true,
+      "enableArithAbort": true
+      }
+}
 
 app.use(cors());
 app.use(express.static('assets'));
 app.options('*', cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/api/images', (request, response) => {
     fs.readdir('assets/images', (err, files) => {
       if (err) {
-        console.log(err)
-        return response.status(404).json({ error: 'content missing' })
+        console.log(err);
+        return response.status(404).json({ error: 'content missing' });
       }
       const urls = files.map(x => backURL + '/images/' + x);
-      response.send(urls)
+      response.send(urls);
     });
   });
 
@@ -35,43 +47,50 @@ app.get('/api/videos', (request, response) => {
 })
 
 app.get('/api/tj', (request, response) => {
-  fs.readFile('assets/tjList.json', (err,data) => {
-    if (err) {
-      console.log(err)
-      return response.status(404).json({ error: 'content missing' })
-    }
-    var tjList = JSON.parse(data)
-    var dateCurrent = new Date()
-    var formattedTjList = tjList.map(tj => {
-      var date = new Date(tj.date)
-      var newTj = {desc: tj.desc, num: Math.ceil((date.getTime() - dateCurrent.getTime()) / (1000 * 3600 * 24))}
-      return newTj
+  sql.connect(sqlConfig, (err) => {
+    if (err) console.log(err)
+    var sqlRequest = new sql.Request();
+    var sqlQuery = 'select * from dbo.Tj'
+    sqlRequest.query(sqlQuery, (err, data) => {
+      if (err) console.log(err);
+
+      var dateCurrent = new Date()
+      var formattedTjList = data.recordset.map(tj => {
+        var date = new Date(tj.DateString)
+        var newTj = {desc: tj.TJDesc, num: Math.ceil((date.getTime() - dateCurrent.getTime()) / (1000 * 3600 * 24))}
+        
+        return newTj
+      })
+      response.send(formattedTjList)
     })
-    response.send(formattedTjList)
   })
 })
 
 app.post('/api/tj', (request, response) => {
   var desc = request.body.desc
   var dateString = request.body.date
-  
-  fs.readFile('assets/tjList.json', (err,data) => {
-    if (err) {
-      console.log(err)
-    }
 
-    var tjList = JSON.parse(data)
-    if (!tjList.find(element => element.desc == desc)){
-      tjList.push({desc: desc, date: dateString})
-      fs.writeFile('assets/tjList.json', JSON.stringify(tjList), (err) => {
-        console.log("New tj saved: ", desc)
-        if (err) {
-          console.log(err)
-        }
-      })
-    }
+  var sqlRequest = new sql.Request();
+  var sqlQuery = 'insert into dbo.Tj values (\'' + desc + '\', \'' + dateString + '\')'
+  
+  sql.connect(sqlConfig, (err) => {
+    if (err) console.log(err);
+
+
+    sqlRequest.query('select * from dbo.Tj', (err, data) => {
+      if (err) console.log(err);
+
+      if (!data.recordset.find( x => x.TJDesc == desc)){
+        sqlRequest.query(sqlQuery, (err) => {
+          if (err) console.log(err)
+          console.log(sqlQuery + " inserted into db")
+        })
+      }
+    
   })
 })
+})
+
 
 app.get('/api/food', (request, response) => {
   fs.readFile('assets/foodMenu.json', (err,data) =>{
@@ -81,24 +100,20 @@ app.get('/api/food', (request, response) => {
 
 app.delete('/api/tj', (request, response) => {
   var deleteDesc = request.body.desc
-  fs.readFile('assets/tjList.json', (err,data) => {
-    if (err) {
-      console.log(err)
-    }
+  var sqlRequest = new sql.Request();
+  var sqlQuery = 'delete from dbo.Tj where TJDesc = \'' + deleteDesc + '\''
 
-    var tjList = JSON.parse(data)
-    var newTjList = tjList.filter (tj =>{
-      return tj.desc !==deleteDesc
-    })
-    fs.writeFile('assets/tjList.json', JSON.stringify(newTjList), (err) => {
+  sql.connect(sqlConfig, (err) => {
+    if (err) console.log(err);
+
+    sqlRequest.query(sqlQuery, (err) => {
+      if (err) console.log(err);
+      
       console.log("Tj deleted: ", deleteDesc)
       response.status(200).json({message: "content removed"})
-      if (err) {
-        console.log(err)
-      }
-      
-      
     })
+
+    
   })
 })
 
